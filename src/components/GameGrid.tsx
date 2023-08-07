443791eeb6cfec0fb84ae6a6849ca2335af4776c
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-07 17:54:22 +0530

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,25 +6,25 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
     <div>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={"10px"}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={3}
         padding={"10px"}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-const config: ThemeConfig ={ 
+const config: ThemeConfig = { 
     initialColorMode: 'dark',
 };
 
