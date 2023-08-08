725020c49ad4a4b4531ca9a1f1021ed2628a5e71
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-08 08:46:10 +0530

    Highlight the selected genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -12,7 +12,7 @@ interface FetchResponse<T>{
 const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
   
     useEffect(() => {
         const controller = new AbortController();
